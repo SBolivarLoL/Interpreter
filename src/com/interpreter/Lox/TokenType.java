7a89beeaa4b4477133a88e5d5a7956545ddb2a97
SBolivarLoL <mikelmd@criptext.com>
2022-12-07 23:38:48 -0500

    code to represent lexemes and tokens

---
+++ src/com/interpreter/Lox/TokenType.java
@@ -0,0 +1,23 @@
+package com.interpreter.Lox;
+
+// We have to categorise the lexemes of our language
+enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords.
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
